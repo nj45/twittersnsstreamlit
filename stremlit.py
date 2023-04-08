7b4be96c8d649b6d9c1250c7e5718a825eddf943
nj45 <43071881+nj45@users.noreply.github.com>
2023-04-09 01:06:47 +0530

    Update stremlit.py

--- stremlit.py
+++ stremlit.py
@@ -1,7 +1,15 @@
-import streamlit as st
+import streamlit as st 
+from twitter import tweets
+# adding a title
 st.title('Twitter scraping Dashboard')
+ 
+  
+#fetching inputs
+
+
 text= st.text_input('Query')
 since = st.date_input('Since')
 until = st.date_input('Until')
 count = st.number_input('count')
+# submit button
 submit = st.button('GO')
